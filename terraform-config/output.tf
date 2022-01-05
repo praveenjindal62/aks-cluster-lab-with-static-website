a4b51ca4eedd31ee44cfe2172d1aaf22f28745ff
Praveen Jindal <praveenjindal62@gmail.com>
2022-01-05 12:55:18 +0530

    Updated output.tf

--- terraform-config/output.tf
+++ terraform-config/output.tf
@@ -1,12 +1,3 @@
-output "acr_admin_username" {
-  value = azurerm_container_registry.acr.admin_username
-}
-
-output "acr_admin_password" {
-  value = azurerm_container_registry.acr.admin_password
-  sensitive = true
-}
-
 output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
 }
