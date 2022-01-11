3c9ce26d65ba306e927861c553b28a4ab50d0b83
Praveen Jindal <praveenjindal62@gmail.com>
2022-01-11 12:14:52 +0530

    Disabled alert in terraform

--- terraform-config/7-alerts.tf
+++ terraform-config/7-alerts.tf
@@ -2,14 +2,13 @@ resource "azurerm_monitor_scheduled_query_rules_alert" "example" {
   name                = "AppErrorAlert"
   location            = azurerm_resource_group.aks_cluster_rg.location
   resource_group_name = azurerm_resource_group.aks_cluster_rg.name
-
   action {
     action_group           = [azurerm_monitor_action_group.actiongroup1.id]
     email_subject          = "Critical App Error"
   }
   data_source_id = azurerm_log_analytics_workspace.lawspace.id
   description    = "Alert when total results cross threshold"
-  enabled        = true
+  enabled        = false
   # Count all errors in container logs in 30 min time window
   query       = <<-QUERY
   ContainerLog
