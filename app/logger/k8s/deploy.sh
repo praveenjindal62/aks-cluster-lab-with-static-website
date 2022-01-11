594dca0fb7d177867d3b2727f0e8db9e265d7fee
Praveen Jindal <praveenjindal62@gmail.com>
2022-01-11 12:17:12 +0530

    Updated logger app deployment

--- app/logger/k8s/deploy.sh
+++ app/logger/k8s/deploy.sh
@@ -2,9 +2,8 @@ script="$0"
 basename="$(dirname $script)"
 cd $basename
 ls -latr
-
-DEPLOYMENT_NAME=$DEPLOYMENT_NAME
-kubectl get deployment $DEPLOYMENT_NAME -o name > /dev/null 2>&1 > /dev/null 2>&1
+DEPLOYMENT_NAME=logger-deploy
+kubectl get deployment $DEPLOYMENT_NAME -o name > /dev/null 2>&1 
 if [ $? -eq 0 ]
 then
     echo "Upgrading deployment"
@@ -12,9 +11,8 @@ then
     kubectl patch deployment $DEPLOYMENT_NAME -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"
 else
     echo "Creating K8s Objects with image $1"
-    sed -i "s/#LOGGERIMAGE#/$1/g" k8s.yml
-    cat k8s.yml
-    kubectl apply -f k8s.yml
+    sed "s~#LOGGERIMAGE#~$1~g" k8s.yml
+    sed "s~#LOGGERIMAGE#~$1~g" k8s.yml | kubectl apply -f -
 fi
 kubectl get all
 
